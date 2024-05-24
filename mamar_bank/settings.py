497d1c2387b46324ebd6c572c0a97cb2d82ab015
saidulvai <saidulvaicsebgctub@gmail.com>
2024-05-25 00:46:35 +0600

    changes_done

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https:///mamar-bank-zr1i.onrender.com/']
+CSRF_TRUSTED_ORIGINS = ['https:///mamar-bank-zr1i.onrender.com']
 
 # Application definition
 
